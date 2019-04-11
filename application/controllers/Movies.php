0a8d78a6ade901dff5fe58ef22685d930e7fb02b
Michael Castro <michaelcastro.js@gmail.com>
2019-04-11 10:52:45 +0300

    fix image issues

--- application/controllers/Movies.php
+++ application/controllers/Movies.php
@@ -6,11 +6,11 @@ class Movies extends CI_Controller {
     public function show_movies() {
     	$data['title'] = ucfirst($this->router->fetch_class()); //Set title to same as this class name with capital letter at first, header.php echoes title value
 
-        $this->load->view('templates/header', $data); //Load generic header
+        // $this->load->view('templates/header', $data); //Load generic header
 
         $this->load->view('movies/show_movies'); //Load actual content for this page
         
-        $this->load->view('templates/footer', $data); //Load generic footer
+        // $this->load->view('templates/footer', $data); //Load generic footer
     
    }
  }
