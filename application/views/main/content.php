cad63670f8392ffa6539d1ff285a697b58bb1fc7
Minsi <k8jami00@students.oamk.fi>
2019-04-21 22:23:20 +0300

    fixcommit

--- application/views/main/content.php
+++ application/views/main/content.php
@@ -3,9 +3,9 @@
 <div>
 <div class="side-menu">
 <ul>
-<li><a href="<?php echo site_url('movies/browse'); ?>">Browse movies</a></li>
+<li><a href="<?php echo site_url('movies/show_movies'); ?>">Browse movies</a></li>
 <li><a href="<?php echo site_url('movies/search'); ?>">Search movies</a></li>
-<?php 
+<?php
     if(empty($_SESSION['user_logged_in']) == false && $_SESSION['user_logged_in'] == true) {
         //User logged, show admin menu link
         echo "<li><a href=\"" . site_url('access/admin_menu') . "\">Administrator menu</a></li>";
