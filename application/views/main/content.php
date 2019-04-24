dab96188f137cc8b65f0507dfca9ef0d524fc22e
Minsi <k8jami00@students.oamk.fi>
2019-04-24 23:03:04 +0300

    add menue for shub

--- application/views/main/content.php
+++ application/views/main/content.php
@@ -4,7 +4,8 @@
 <div class="side-menu">
 <ul>
 <li><a href="<?php echo site_url('movies/show_movies'); ?>">Show movies</a></li>
-<?php 
+<li><a href="<?php echo site_url('movies/show_movies'); ?>">Shub</a></li>
+<?php
 
     if(empty($_SESSION['user_logged_in']) == false && $_SESSION['user_logged_in'] == true) {
         //User logged, show admin menu link
