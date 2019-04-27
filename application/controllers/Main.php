87151eed7cd8616e468133ba320ce9b65dafc3d4
Minsi <k8jami00@students.oamk.fi>
2019-04-27 21:52:28 +0300

    add css and javascript

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,8 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
     public function index() {
+        $this->load->model('Main_model');
+
         $data['title'] = ucfirst($this->router->fetch_class()); //Set title to same as this class name with capital letter at first, header.php echoes title value
+
+        //Get 5 random movie posters from database
+        $data['posters'] = $this->Main_model->getRandomMoviePosters(5);
 
         $this->load->view('templates/header', $data); //Load generic header
 
--- application/views/main/content.php
+++ application/views/main/content.php
@@ -4,7 +4,7 @@
 <div class="side-menu">
 <ul>
 <li><a href="<?php echo site_url('movies/show_movies'); ?>">Show movies</a></li>
-<li><a href="<?php echo site_url('movies/show_movies'); ?>">Shub</a></li>
+<li><a href="<?php echo site_url('movies/show_movies'); ?>">Trivia</a></li>
 <?php
 
     if(empty($_SESSION['user_logged_in']) == false && $_SESSION['user_logged_in'] == true) {
@@ -16,5 +16,45 @@
 </div>
 <div class="main-content">
 To start, choose wanted action from the left side menu.
+
+<br>
+<br>
+<div class="slide-image">
+    <img class="slide-image" src="" id="slide-image">
+</div>
+<script>
+var posters = [<?php
+$counter = 0;
+foreach ($posters as $poster) {
+    if($counter != 0) {
+        echo ", ";
+    }
+    //Note, we need to remove "../../" from poster address to get correct url with base_url
+    echo "\"" . base_url(str_replace('../', '', $poster)) . "\"";
+    $counter++;
+}
+?>
+]
+
+//Simple image fading javascript, when changing image, set image opacity to 0 and then to 1
+var slideIndex = 0;
+fadeIn();
+
+function fadeOut() {
+    document.getElementById("slide-image").style.opacity = "0";
+    setTimeout(fadeIn, 1200);
+}
+
+function fadeIn() {
+    document.getElementById("slide-image").src = posters[slideIndex];
+    document.getElementById("slide-image").style.opacity = "1";
+    slideIndex++;
+    if(slideIndex > posters.length-1) {
+        slideIndex = 0
+    }
+    setTimeout(fadeOut, 5000);
+}
+</script>
+
 </div>
 </div>
